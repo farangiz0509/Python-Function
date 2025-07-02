c225435ac494d3f62ead32fdb1c03a8a722199c2
farangiz0509 <farangiztolibova75@gmail.com>
2025-07-02 10:40:09 +0545

    done

--- practice03.py
+++ practice03.py
@@ -1,5 +1,28 @@
-a = 800.000
-result =( 800.000 * 20)/100
-print(result)
+
+
+def is_even(number):
+    return number % 2 == 0
+    
+    
+def print_even_message(number):
+
+    print (f" {number} is even num")
+    
+def print_odd_message(number):
+    
+    print(f"{number} is odd num ")
+    
+def main():
+    number = int(input ("enter the number"))
+    if is_even(number):
+     print_even_message(number)
+         
+          
+    else :
+      print_odd_message(number)
+          
+main()    
+
+    
 
      
