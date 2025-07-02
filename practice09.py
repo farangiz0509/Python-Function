2d1bca17a3ff428562c1ee6b2379a0b7fbc25036
farangiz0509 <farangiztolibova75@gmail.com>
2025-07-02 13:12:06 +0545

    done

--- practice09.py
+++ practice09.py
@@ -1,5 +1,22 @@
 def deposit(balance, amount):
+    if amount > 0:
+        balance += amount
+        return balance
      
 def withdraw(balance, amount):
+    if amount <= balance:
+        balance-= amount
+        return balance
      
 def check_balance(balance):
+    print(f"balance : {balance}")
+    
+hisob = 100
+
+hisob = deposit (hisob , 30)
+check_balance(hisob)
+
+hisob = withdraw(hisob, 70)
+
+check_balance(hisob)
+
